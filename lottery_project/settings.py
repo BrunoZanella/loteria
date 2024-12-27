ee7f3f47f910d91c7571a93d2539f1707b6771e5
BrunoZanella <zanellabruno7@gmail.com>
2024-12-27 13:45:59 -0300

    atualizacao de email de recuperacao

--- lottery_project/settings.py
+++ lottery_project/settings.py
@@ -112,3 +112,13 @@ CRISPY_TEMPLATE_PACK = "bootstrap5"
 
 LOGIN_REDIRECT_URL = '/'
 LOGOUT_REDIRECT_URL = '/'
+
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = 'zanellabruno7@gmail.com'
+EMAIL_HOST_PASSWORD = 'wron fcmr ugbj ufhb'
+DEFAULT_FROM_EMAIL = 'zanellabruno7@gmail.com'
+ADMINS = [('Admin Zanella', 'zanellabruno7@gmail.com')]
